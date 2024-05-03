b1d21e1c81e8a1be6ee3dacb7381e8f6078c34a9
Aminat Adebayo <adebayo.aminat@gmail.com>
2024-05-03 14:46:57 -0400

    clean up

--- server/seed.py
+++ server/seed.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 # Standard library imports
 from random import randint, choice as rc
 
@@ -9,6 +7,19 @@ from faker import Faker
 # Local imports
 from app import app
 from models import db
+from models import User
+
+
+def seed():
+    # delete current data
+    User.query.delete()
+    
+    # create admin user for testing and debugging
+    admin = User(username="admin", admin=True)
+    admin.password_hash = 'admin'
+
+    db.session.add(admin)
+    db.session.commit()
 
 if __name__ == '__main__':
     fake = Faker()
