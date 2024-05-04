07f07ec8972570a31369c27c23136af9c5a5a5de
Aminat Adebayo <adebayo.aminat@gmail.com>
2024-05-04 07:47:27 -0400

    updates

--- client/src/components/Home/Signup.js
+++ client/src/components/Home/Signup.js
@@ -3,6 +3,15 @@ import React,{ useRef, useState, useEffect} from 'react'
 function Signup() {
   const [user, setUser] = useState("")
 
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
 
   return (
     <div>
